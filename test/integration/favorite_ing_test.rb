ef83d13a7205e5f7d01e2034e7cd8557bc9aff20
miki <mikannoishiii@gmail.com>
2020-02-10 14:48:02 +0900

    favorite 途中まで

--- test/integration/favorite_ing_test.rb
+++ test/integration/favorite_ing_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class FavoriteIngTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @post = posts(:ants)
     log_in_as(@user)
   end
 
@@ -14,4 +15,32 @@ class FavoriteIngTest < ActionDispatch::IntegrationTest
       assert_select "li#post-#{post.id}"
     end
   end
+
+  test "should like a poat the standard way" do
+    assert_difference '@user.favorites.count', 1 do
+      post favorites_path, params: { post_id: @post.id }
+    end
+  end
+
+  # test "should like a post with Ajax" do
+  #   assert_difference '@user.favorites.count', 1 do
+  #     post favorites_path, xhr: true, params: { post_id: @post.id }
+  #   end
+  # end
+
+  test "should unlike a post the standard way" do
+    @user.like(@post)
+    favorite = @user.favorites.find_by(post_id: @post.id)
+    assert_difference '@user.favorites.count', -1 do
+      delete favorite_path(favorite)
+    end
+  end
+
+  # test "should unlike a post with Ajax" do
+  #   @user.unlike(@post)
+  #   favorite = @user.favorites.find_by(post_id: @post.id)
+  #   assert_difference '@user.favorites.count', -1 do
+  #     delete favorite_path(favorite), xhr: true
+  #   end
+  # end
 end
